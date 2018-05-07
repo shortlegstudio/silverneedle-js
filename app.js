cc07718575047e0f87b156af0de505c8e0baee00
Trevor Redfern <trevorredfern@gmail.com>
2018-05-06 19:14:29 -0700

    Does this work?

--- app.js
+++ app.js
@@ -38,8 +38,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 module.exports = app;
